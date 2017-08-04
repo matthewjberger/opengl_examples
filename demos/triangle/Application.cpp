e28715ea77cc7574335dc943813833f8ed90f790
Matthew J. Berger <matthewberger@nevada.unr.edu>
2017-08-04 16:01:51 -0700

    Update triangle demo.

--- demos/triangle/Application.cpp
+++ demos/triangle/Application.cpp
@@ -74,7 +74,7 @@ void Application::initialize()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
